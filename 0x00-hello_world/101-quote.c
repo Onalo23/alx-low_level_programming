0b6034de13443cd8e709f4d490413413094b16f3
Onalo23 <charlearinze7@gmail.com>
2023-02-17 03:46:02 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - C programme that print to the standard error
- *
+ * main - Entry point
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", stdout);
-    return (1);
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
