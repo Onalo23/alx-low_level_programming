ab9668218d295edc3e6b430d3ee3594f195c1311
Onalo23 <charlearinze7@gmail.com>
2023-02-18 03:41:47 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - Entry point
  * Return: Always 0 (success)
@@ -6,11 +7,13 @@
 
 int main(void)
 {
-	char lc;
-	for(lc = 'a'; lc <= 'z'; lc++)
+	char c;
+	c = 'a';
+      while (c <= 'z')
 	{
- 	 putchar(lc);
-		}
+ 		 putchar(c);
+		 c++;
+	}
 		putchar('\n');
 	return (0);
 }
