6d75230b8481050768c7a00f26d866eaffdcc980
Onalo23 <charlearinze7@gmail.com>
2023-02-21 13:00:56 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -4,7 +4,6 @@
  * _putchar - writes the character c to stdout
  * @c: the character to print
  * Return: on success 1
- * on error, -1 is return, and errno is set appropriately
  */
 int _putchar(char c)
 {
