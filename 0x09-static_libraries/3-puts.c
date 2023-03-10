a53dd52c2895da30f46565d95916408a08c4b30e
Onalo23 <charlearinze7@gmail.com>
2023-03-10 07:57:00 -0800

    create_static_lib.sh

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,13 @@
 #include "main.h"
-#include <stdio.h>
-
+void _puts(char *str)
+#include<stdio.h>
 /**
- * main - prints "Programming is like building a multilingual puzzle
- * followed by new lins.
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-int main(void)
+void _puts(char *str)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	return (0);
 }
