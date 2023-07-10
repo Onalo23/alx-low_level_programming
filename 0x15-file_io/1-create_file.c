8bf4ab94409992e550e11cf54f64ea34c8b9cd81
Onalo23 <charlesarinze7@gmail.com>
2023-07-10 12:03:52 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * create_file - the file creator
+ * @filename: the file pointer to be created
+ * @text_content: the file sting pointer to be written
+ * Return: failure -1, otherwise 1
+ */
+
+int create_file(const char *filename, char *text_content)
+
+{
+int bd, z, len = 0;
+if (filename == NULL)
+return (-1);
+if (text_content != NULL)
+{
+for (len = 0; text_content[len];)
+len++;
+}
+bd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+z = write(bd, text_content, len);
+if (bd == -1 || z == -1)
+return (-1);
+close(bd);
+return (1);
+}
